2b46575eac4e78ec283e0e108454e842a4192b5d
DhilipSanjay <dhilip.sanjay@yahoo.com>
2020-01-20 16:55:55 +0530

    publisher completed

---
+++ WebApplication2/publisher.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports MySql.Data.MySqlClient
+
+
+Public Class publisher
+    Inherits System.Web.UI.Page
+    Dim con As New MySqlConnection("server=127.0.01;user id=root;pwd=sanjay2001;database=jms")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Save.Click
+        If pubName.Text = "" Or address.Text = "" Or pincode.Text = "" Then
+            MsgBox("Please fill the mandatory fields", 0, "Attention Required")
+        End If
+        con.Open()
+        Dim cmd As MySqlCommand
+        Dim valuestr As String
+        valuestr = "insert into publisher(pubName,address,pincode,country,email,phone,fax,website,contactPerson) values('" & pubName.Text & "','" & address.Text & "'," & Val(pincode.Text) & ",'" & country.Text & "','" & email.Text & "'," & Val(phone.Text) & "," & Val(fax.Text) & ",'" & website.Text & "','" & contactPerson.Text & "')"
+        cmd = New MySqlCommand(valuestr, con)
+        cmd.ExecuteNonQuery()
+        con.Close()
+    End Sub
+
+    Protected Sub pubName_TextChanged(sender As Object, e As EventArgs) Handles pubName.TextChanged
+
+    End Sub
+
+    Protected Sub country_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
