5d1c73ec0557a6a4584f829730d093fabf6815f2
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-01-31 08:35:16 +0530

    disappearing buttons and inward insert delete

--- WebApplication2/InwardEntry.aspx.vb
+++ WebApplication2/InwardEntry.aspx.vb
@@ -23,7 +23,7 @@ Public Class InwardEntry
                 cmd.ExecuteNonQuery()
                 con.Close()
                 MsgBox("Saved successfully")
-
+                Button2_Click(Nothing, Nothing)
 
             Catch ex As MySqlException
                 If ex.Number = 3819 Then
@@ -50,6 +50,7 @@ Public Class InwardEntry
     End Sub
 
     Protected Sub codeBox_TextChanged(sender As Object, e As EventArgs) Handles codeBox.TextChanged
+        'needs changes
         Try
             con.Open()
             Dim command As String
@@ -61,14 +62,82 @@ Public Class InwardEntry
                 TitleBox.Text = dr(0)
                 VolNoBox.Text = dr(1)
                 IssueNoBox.Text = dr(2)
-            Else
-                MsgBox("J code Not available")
-            End If
 
+            End If
             con.Close()
-            Button2_Click(Nothing, Nothing)
+
         Catch ex As Exception
-            MsgBox("Error in getting title " & ex.ToString)
+            MsgBox("Error In getting title " & ex.ToString)
         End Try
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'update
+        If codeBox.Text IsNot "" And VolNoBox.Text IsNot "" And IssueNoBox.Text IsNot "" Then
+
+            Dim ch As Int16
+            ch = MsgBox("Are you sure you want To update this entry?", 3, "Confirm Update")
+            Select Case ch
+                Case vbYes
+                    Try
+                        con.Open()
+                        Dim cmdstr As String
+                        cmdstr = "Update inwardentry Set fromdate='" & FromDateBox.Text & "',todate='" & ToDateBox.Text & "',mergeremark='" & MergeRemarkBox.Text & "' where jcode = " & codeBox.Text & " and volno = " & VolNoBox.Text & " and issueno = " & IssueNoBox.Text
+                        cmd = New MySqlCommand(cmdstr, con)
+                        cmd.ExecuteNonQuery()
+                        con.Close()
+                        MsgBox("Modified successfully")
+                        Button2_Click(Nothing, Nothing)
+
+                    Catch ex As MySqlException
+
+                        MsgBox("error" & ex.ToString)
+
+
+                    End Try
+
+
+            End Select
+
+
+        Else
+            MsgBox("Fill mandatory details", 0, "Missing Field")
+        End If
+
+    End Sub
+
+    Protected Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'delete
+        If codeBox.Text IsNot "" And VolNoBox.Text IsNot "" And IssueNoBox.Text IsNot "" Then
+
+            Dim ch As Int16
+            ch = MsgBox("Are you sure you want to delete this entry?", 3, "Confirm Delete")
+            Select Case ch
+                Case vbYes
+                    Try
+                        con.Open()
+                        Dim cmdstr As String
+                        cmdstr = "Delete from inwardentry where jcode = " & codeBox.Text & " and volno = " & VolNoBox.Text & " and issueno = " & IssueNoBox.Text
+                        cmd = New MySqlCommand(cmdstr, con)
+                        cmd.ExecuteNonQuery()
+                        con.Close()
+                        MsgBox("Deleted successfully")
+                        Button2_Click(Nothing, Nothing)
+
+                    Catch ex As MySqlException
+
+                        MsgBox("error" & ex.ToString)
+
+
+                    End Try
+
+
+            End Select
+
+
+        Else
+            MsgBox("Fill mandatory fields", 0, "Missing Field")
+        End If
+
     End Sub
 End Class
--- WebApplication2/publisher.aspx.vb
+++ WebApplication2/publisher.aspx.vb
@@ -5,8 +5,34 @@ Public Class publisher
     Inherits System.Web.UI.Page
     Dim constr As String = ConfigurationManager.ConnectionStrings("jmsConnectionString2").ConnectionString
     Dim con As New MySqlConnection(constr)
+    Dim newid As Int16
+
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+                con.Open()
+                Dim cmd As New MySqlCommand
+                Dim cmdstr As String
+                cmdstr = "select max(pubid)+1 from publisher;"
+                cmd = New MySqlCommand(cmdstr, con)
+                Dim dr As MySqlDataReader
+                dr = cmd.ExecuteReader()
+                If dr.Read() Then
+                newid = dr(0)
+                If Not Me.IsPostBack Then
+                    pubId.Text = newid
+                    pubId_TextChanged(Nothing, Nothing)
+                End If
+
+
+            End If
+                dr.Close()
+                con.Close()
+            Catch ex As MySqlException
+                MsgBox("Error" & ex.ToString)
+            End Try
+
 
     End Sub
 
@@ -38,5 +64,91 @@ Public Class publisher
         country.Text = ""
         fax.Text = ""
         contactPerson.Text = ""
+    End Sub
+
+    Protected Sub pubId_TextChanged(sender As Object, e As EventArgs) Handles pubId.TextChanged
+
+
+        If pubId.Text >= newid Then
+            clearContents_Click(Nothing, Nothing)
+            Save.Enabled = True
+            Save.Visible = True
+            update.Enabled = False
+            update.Visible = False
+            deleteRecord.Visible = False
+            deleteRecord.Enabled = False
+
+        Else
+
+            Save.Enabled = False
+            Save.Visible = False
+            update.Enabled = True
+            update.Visible = True
+            deleteRecord.Visible = True
+            deleteRecord.Enabled = True
+            Try
+                con.Open()
+                Dim cmd As New MySqlCommand
+                Dim cmdstr As String
+                cmdstr = "select pubname , address,pincode,phone,email,website,country,fax,contactPerson from publisher where pubid = " & pubId.Text
+                cmd = New MySqlCommand(cmdstr, con)
+                Dim dr As MySqlDataReader
+                dr = cmd.ExecuteReader()
+                If dr.Read() Then
+                    clearContents_Click(Nothing, Nothing)
+                    pubName.Text = dr(0)
+                    address.Text = dr(1)
+                    pincode.Text = dr(2)
+                    phone.Text = dr(3)
+                    email.Text = dr(4)
+                    website.Text = dr(5)
+                    country.Text = dr(6)
+                    fax.Text = dr(7)
+                    contactPerson.Text = dr(8)
+
+                End If
+                dr.Close()
+                con.Close()
+            Catch ex As MySqlException
+                MsgBox(ex.ToString)
+            Catch other As Exception
+                MsgBox(other.ToString)
+            End Try
+
+        End If
+
+
+    End Sub
+
+    Protected Sub deleteRecord_Click(sender As Object, e As EventArgs) Handles deleteRecord.Click
+        If pubId.Text = "" Then
+            MsgBox("Enter Pubid", 0, "Mandatory Field")
+        Else
+            Dim ch As Int16
+            ch = MsgBox("Are you sure you want to delete this entry?", 3, "Confirm Delete")
+            If ch = vbYesNo Then
+                Try
+                    con.Open()
+                    Dim cmd As MySqlCommand
+                    Dim cmdstr As String
+                    cmdstr = "Delete from publisher where pubid = " & pubId.Text
+                    cmd = New MySqlCommand(cmdstr, con)
+                    cmd.ExecuteNonQuery()
+                    con.Close()
+                    MsgBox("Deleted successfully")
+                    clearContents_Click(Nothing, Nothing)
+
+                Catch ex As MySqlException
+
+                    MsgBox("error" & ex.ToString)
+
+
+                End Try
+
+            End If
+
+
+        End If
+
     End Sub
 End Class
