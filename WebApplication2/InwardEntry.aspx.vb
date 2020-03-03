3c435cfd00b688a7a152748bce2bc45069054a18
SwethaMagesh <43994542+SwethaMagesh@users.noreply.github.com>
2020-03-03 15:08:42 +0530

    Report init

--- WebApplication2/InwardEntry.aspx.vb
+++ WebApplication2/InwardEntry.aspx.vb
@@ -6,9 +6,9 @@ Public Class InwardEntry
     Dim cmd As New MySqlCommand
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-
-
+        If Not Me.IsPostBack Then
+            FromDateBox.Text = DateTime.Today.ToString("yyyy-MM-dd")
+        End If
     End Sub
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -26,6 +26,7 @@ Public Class InwardEntry
                 Button2_Click(Nothing, Nothing)
 
             Catch ex As MySqlException
+                con.Close()
                 If ex.Number = 3819 Then
                     MsgBox("Date constraint ")
                 Else
@@ -44,7 +45,7 @@ Public Class InwardEntry
         TitleBox.Text = ""
         VolNoBox.Text = ""
         IssueNoBox.Text = ""
-        FromDateBox.Text = ""
+        FromDateBox.Text = Today.ToString("yyyy-MM-dd")
         ToDateBox.Text = ""
         MergeRemarkBox.Text = ""
     End Sub
@@ -54,22 +55,63 @@ Public Class InwardEntry
         Try
             con.Open()
             Dim command As String
-            command = "select m.title,i.volno,i.issueno from master m,inwardentry i where m.code =" & codeBox.Text & " and i.jcode =" & codeBox.Text & " order by i.volno desc,i.issueno desc limit 1;"
+            command = "select volno,issueno,fromdate,todate,mergeremark from inwardentry where jcode=" & codeBox.Text & " order by volno desc,issueno desc limit 1;"
             cmd = New MySqlCommand(command, con)
             Dim dr As MySqlDataReader
             dr = cmd.ExecuteReader()
             If dr.Read() Then
-                TitleBox.Text = dr(0)
-                VolNoBox.Text = dr(1)
-                IssueNoBox.Text = dr(2)
 
+                VolNoBox.Text = dr(0).ToString
+                IssueNoBox.Text = dr(1).ToString
+                Dim fd As Date
+                fd = dr(2)
+                Dim td As Date
+                td = dr(3)
+                FromDateBox.Text = fd.ToString("yyyy-MM-dd")
+                ToDateBox.Text = td.ToString("yyyy-MM-dd")
+                MergeRemarkBox.Text = dr(4)
+
+            End If
+            dr.Close()
+
+            command = "select title from master where code =" & codeBox.Text
+            cmd = New MySqlCommand(command, con)
+            dr = cmd.ExecuteReader()
+            If dr.Read() Then
+                TitleBox.Text = dr(0)
+            Else
+                Button2_Click(Nothing, Nothing)
             End If
             con.Close()
 
         Catch ex As Exception
-            MsgBox("Error In getting title " & ex.ToString)
+            con.Close()
+
+            MsgBox("Error In getting details " & ex.ToString)
         End Try
     End Sub
+    Function isIssuePresent()
+        Dim flag As Boolean
+        Try
+            con.Open()
+            Dim cmdstr As String
+            cmdstr = "select volno,issueno from inwardentry where jcode= " & codeBox.Text & " and issueno= " & IssueNoBox.Text & " and volno =" & VolNoBox.Text
+            cmd = New MySqlCommand(cmdstr, con)
+            Dim dr As MySqlDataReader
+            dr = cmd.ExecuteReader()
+            If dr.Read Then
+                flag = 1
+            Else
+                flag = 0
+            End If
+
+            con.Close()
+            Return flag
+        Catch ex As MySqlException
+            MsgBox("error" & ex.ToString)
+        End Try
+        Return -1
+    End Function
 
     Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         'update
@@ -90,8 +132,12 @@ Public Class InwardEntry
                         Button2_Click(Nothing, Nothing)
 
                     Catch ex As MySqlException
-
-                        MsgBox("error" & ex.ToString)
+                        con.Close()
+                        If ex.Number = 3819 Then
+                            MsgBox("Date constraint ")
+                        Else
+                            MsgBox("error" & ex.ToString)
+                        End If
 
 
                     End Try
@@ -125,6 +171,7 @@ Public Class InwardEntry
                         Button2_Click(Nothing, Nothing)
 
                     Catch ex As MySqlException
+                        con.Close()
 
                         MsgBox("error" & ex.ToString)
 
@@ -137,6 +184,60 @@ Public Class InwardEntry
 
         Else
             MsgBox("Fill mandatory fields", 0, "Missing Field")
+        End If
+
+    End Sub
+
+    Protected Sub IssueNoBox_TextChanged(sender As Object, e As EventArgs) Handles IssueNoBox.TextChanged
+        Dim issuePresent As Boolean
+
+        issuePresent = isIssuePresent()
+        If Not issuePresent Then
+
+            MsgBox("no issue")
+            Button1.Enabled = True
+            Button1.Visible = True
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button3.Visible = False
+            Button4.Visible = False
+        Else
+            MsgBox("issued")
+            Button3.Enabled = True
+            Button4.Enabled = True
+            Button3.Visible = True
+            Button4.Visible = True
+            Button1.Enabled = False
+            Button1.Visible = False
+            Try
+                con.Open()
+                Dim command As String
+                command = "select volno,issueno,fromdate,todate,mergeremark from inwardentry where jcode=" & codeBox.Text & "  and volno = " & VolNoBox.Text & " and issueno = " & IssueNoBox.Text
+                cmd = New MySqlCommand(command, con)
+                Dim dr As MySqlDataReader
+                dr = cmd.ExecuteReader()
+                If dr.Read() Then
+
+                    'VolNoBox.Text = dr(0).ToString
+                    'IssueNoBox.Text = dr(1).ToString
+                    Dim fd As Date
+                    fd = dr(2)
+                    Dim td As Date
+                    td = dr(3)
+                    FromDateBox.Text = fd.ToString("yyyy-MM-dd")
+                    ToDateBox.Text = td.ToString("yyyy-MM-dd")
+                    MergeRemarkBox.Text = dr(4)
+
+                End If
+                dr.Close()
+                con.Close()
+
+
+            Catch ex As Exception
+                con.Close()
+
+                MsgBox("Error In getting details " & ex.ToString)
+            End Try
         End If
 
     End Sub
---
+++ WebApplication2/Report1.aspx.vb
@@ -0,0 +1,58 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Report1
+    Inherits System.Web.UI.Page
+    Dim constr As String = ConfigurationManager.ConnectionStrings("jmsConnectionString2").ConnectionString
+    Dim con As New MySqlConnection(constr)
+    Dim cmd As New MySqlCommand
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub obtain_Click(sender As Object, e As EventArgs) Handles obtain.Click
+        Dim val1 As String
+        Dim val2 As String
+        Dim val3 As String
+
+        Select Case List2.SelectedValue
+            Case 0
+                val2 = "publisher"
+                val1 = " pubid as PubId, pubname as Publisher"
+
+            Case 1
+                val2 = " master "
+                val1 = "code as JournalCode, title as JournalTitle"
+
+            Case 2
+                val2 = "subscription "
+                val1 = " "
+            Case Else
+                val1 = ""
+                val2 = " "
+        End Select
+
+        If List1.SelectedValue = 0 Then
+            val1 = " Count(*)  as Count "
+
+        End If
+
+
+        con.Open()
+        Try
+            Dim cmdstr As String
+            cmdstr = " select " & val1 & "  from " & val2
+            cmd = New MySqlCommand(cmdstr, con)
+            Dim da As MySqlDataAdapter
+            Dim dt As New DataTable()
+            da = New MySqlDataAdapter(cmd)
+            da.Fill(dt)
+            GridView1.DataSource = dt
+            GridView1.DataBind()
+        Catch ex As MySqlException
+            MsgBox("Error " & ex.ToString)
+        End Try
+
+
+        con.Close()
+
+    End Sub
+End Class
